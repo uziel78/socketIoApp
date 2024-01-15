6648ff9434fd8251312a2e35efcbe02ee21b8998
uziel78 <rune.husa78@gmail.com>
2024-01-15 20:09:35 +0100

    saving rooms in json file

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ const io = new Server(server);
 const path = require('path');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
+const fs = require('fs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+let rooms = JSON.parse(fs.readFileSync('./rooms.json', 'utf-8'));
 
 app.get('/', (req, res) => {
   res.render(__dirname + '/index.ejs', { rooms: rooms });
 });
 
 // rooms array
-let rooms = ['room1', 'room2'];
+//let rooms = ['room1', 'room2'];
 
 app.get('/room1', (req, res) => {
   res.render(__dirname + '/room.ejs', { room: 'room1' });
@@ -33,14 +36,24 @@ app.get('/room2', (req, res) => {
 //newroom POST handler:
 app.post('/newroom', jsonParser, (req, res) => {
   const room = req.body.room;
-  //const color = req.body.color;
   app.get('/' + room, (req, res) => {
     res.render(__dirname + '/room.ejs', { room: room });
   });
-  rooms.push(room);
-  res.send({
-    room: room,
-  });
+  if (!rooms.includes(req.body.room)) {
+    rooms.push(room);
+    if (req.body.save) {
+      let rooms = JSON.parse(fs.readFileSync('./rooms.json', 'utf-8'));
+      const newRooms = rooms.concat([req.body.room]);
+      fs.writeFileSync('./rooms.json', JSON.stringify(newRooms));
+    }
+    res.send({
+      room: room,
+    });
+  } else {
+    res.send({
+      error: 'room already exist',
+    });
+  }
 });
 
 const admin = io.of('/admin');
